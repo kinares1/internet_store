31bd89734d33d392a2e58fd5aea2d5d1c7fc98e6
Denis <116779712+kinares1@users.noreply.github.com>
2023-05-26 01:32:44 +0700

    Add files via upload

---
+++ internet_store_app/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=100)
+    registration_date = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100)
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"Order #{self.id}"
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    objects = models.Manager()
+
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name}"
